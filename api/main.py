ad216fea6b1f78615e7d70caee441ffe972bd551
Randy Nance <randynobx@gmail.com>
2022-10-21 14:36:08 -0400

    Removed graph_router.py calls

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from api.data import BoardgamesDB
-from api.routers import artists_router, categories_router, designers_router, games_router, graph_router, \
+from api.routers import artists_router, categories_router, designers_router, games_router, \
     mechanics_router, publishers_router
 
 API = FastAPI(
@@ -19,6 +19,5 @@ async def version():
     return {"result": {"Version": API.version}}
 
 
-for router in (artists_router, categories_router, designers_router, games_router, graph_router, mechanics_router,
-               publishers_router):
+for router in (artists_router, categories_router, designers_router, games_router, mechanics_router, publishers_router):
     API.include_router(router.Router)
